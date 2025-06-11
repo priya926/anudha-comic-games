890b50121558750dcedf916a99d24ba931f3ea90
priya926 <anubhutisingh926@gmail.com>
2025-06-11 14:37:15 +0530

    firebase changes

--- anudhaGames/firebase_config.py
+++ anudhaGames/firebase_config.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, Firestore
 # Check if Firebase is already initialized
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("anudhadb-firebase-adminsdk-fbsvc-b636f66480.json")   
+    cred = credentials.Certificate("/etc/secrets/anudhadb-firebase-adminsdk-fbsvc-b636f66480.json")   
     firebase_admin.initialize_app(cred)
 
 # Firestore database instance
