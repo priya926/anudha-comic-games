f108d1e620f5fd30614e8d6e0994da8775d8fad5
priya926 <anubhutisingh926@gmail.com>
2025-06-11 14:08:26 +0530

    settings change

--- anudhaGames/anudhaGames/settings.py
+++ anudhaGames/anudhaGames/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-a260j4&kn5=9mt(%r541!_2i)@gnv2gon-r%o_loo$c8b4dso1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anudhagames.onrender.com']
 
 
 # Application definition
