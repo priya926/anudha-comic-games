40dd9bb0b35eca96849c807fd0537fbfd92e34c1
priya926 <anubhutisingh926@gmail.com>
2025-06-11 11:39:15 +0530

    update firebase config

--- anudhaGames/firebase_config.py
+++ anudhaGames/firebase_config.py
@@ -1,10 +1,18 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
 
 # Check if Firebase is already initialized
 if not firebase_admin._apps:
-    cred = credentials.Certificate("anudhadb-firebase-adminsdk-fbsvc-b636f66480.json")
-    firebase_admin.initialize_app(cred)
+    firebase_json = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+    
+    if firebase_json:
+        cred_dict = json.loads(firebase_json)
+        cred = credentials.Certificate(cred_dict)
+        firebase_admin.initialize_app(cred)
+    else:
+        raise ValueError("Firebase credentials not found in environment.")
 
 # Firestore database instance
 db = firestore.client()
